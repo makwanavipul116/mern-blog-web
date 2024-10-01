c8583795ec3ca1aa46af4bc6a1272713d5d17649
makwanavipul116 <167556380+makwanavipul116@users.noreply.github.com>
2024-10-01 20:26:34 +0530

    Update configDB.js

--- api/db/configDB.js
+++ api/db/configDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const connectDb = async () => {
-    consle.log(process.env.MONGO_URL)
+    console.log(process.env.MONGO_URL)
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`mongodb connected on ${conn.connection.host}`)
