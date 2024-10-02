54866fbafda7db923c91e06599fa260550e7f0d6
makwanavipul116 <167556380+makwanavipul116@users.noreply.github.com>
2024-10-02 15:50:33 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -75,12 +75,12 @@ export default function Header() {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                <AiOutlineSearch />
+            <Button className='w-12 h-10 lg:hidden' color='gray' pill onClick={() => navigate(`/search`)}>
+                <AiOutlineSearch size='15px' />
             </Button>
             <div className='flex gap-2 md:order-2'>
                 <Button
-                    className='w-12 h-10 hidden sm:inline'
+                    className='w-12 h-10 '
                     color='gray'
                     pill
                     onClick={() => dispatch(toggleTheme())}
